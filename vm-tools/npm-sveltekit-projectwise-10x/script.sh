65ebcd7370d7d025c879e33f81c12bea9c8b4305
Asko Kauppi <akauppi@gmail.com>
2025-06-13 12:48:43 +0300

    interim; reshaping the 'npm' optimization instructions

--- vm-tools/npm-sveltekit-projectwise-10x/script.sh
+++ vm-tools/npm-sveltekit-projectwise-10x/script.sh
@@ -20,36 +20,114 @@ set -e
 
 # Pick a random string and map to there
 NODE_MODULES_ROOT=$HOME/.node_modules
-NODE_MODULES_BIND="$NODE_MODULES_ROOT/$(openssl rand -hex 12)"
+NODE_MODULES_BIND="$NODE_MODULES_ROOT/$(openssl rand -hex 4)"
+
+SVELTEKIT_RAND=$(openssl rand -hex 4)
 
 # Make sure folders we need do exist; allows running before 'npm build' or similar.
-install -d $NODE_MODULES_ROOT node_modules \
+install -d node_modules \
   .svelte-kit
 
 cat <<EOF
 
-  To improve 'npm' performance, run the following:
+  1. SHADOW 'node_modules' on VM local
 
-  install -d $NODE_MODULES_BIND
-  sudo mount --bind $NODE_MODULES_BIND node_modules
+    (( Move to your project folder (the one with 'package.json'). ))
 
-  If you use SvelteKit, also run:
+    We'll tie a random string to the mount. Let it be "$NODE_MODULES_BIND" (you can use any!).
 
-  sudo mount -t tmpfs -o size=5m,uid=1000 \$(openssl rand -hex 12) .svelte-kit
+    1.1 Make sure the mount point exists
 
-  sudo cat /etc/mtab | grep \$(pwd)/
-  <<
-  /dev/sda1 /home/ubuntu/FinalYards_website/node_modules ext4 rw,relatime,discard,errors=remount-ro,commit=30 0 0
-  d1faf8d85b09aa0edf11349f /home/ubuntu/FinalYards_website/.svelte-kit tmpfs rw,relatime,size=5120k,uid=1000,inode64 0 0
-  <<
+      install -d node_modules
 
-  Now, append those lines to '/etc/fstab':
+    1.2 Create the mount target; this is within the local VM partition, so will have "native" speed:
 
-  sudo nano /etc/fstab
+      install -d $NODE_MODULES_BIND
 
-  Restart the VM. See that the mounts remain:
+    1.3 Add an entry to the '/etc/fstab'. This does two things:
+      - allows a mere (owning) user to 'mount' the 'node_modules'
+      - allows us to recreate the mount after VM restarts
 
-  sudo cat /etc/mtab | grep \$(pwd)/
+      <<
+        /home/ubuntu/.node_modules/0f9399e3 /home/ubuntu/FinalYards_website/node_modules none user,noauto,bind,rw,relatime,discard,commit=30 0 0
+      <<
 
-  You should see two lines (same as for the command before the restart).
+      sudo nano /etc/fstab
+
+      Try it:
+
+      sudo systemctl daemon-reload
+      mount node_modules
+
+      mount | grep \$(pwd)/
+
+      You should be seeing an entry about the 'node_modules' folder.
+
+    1.4 To re-mount after VM restarts
+
+      - either manually do the 'mount node_modules', each time, or add it to your '~/.bashrc' (with full path, of course):
+
+      <<
+        mount $HOME/{your-folder}/node_modules
+      <<
+
+  2. If you use SvelteKit, make a 'tmpfs' mount (empty after each VM restart):
+
+    1.1 Make sure the mount point exists
+
+      install -d .svelte-kit
+
+    2.2 Add an entry to '/etc/fstab':
+
+      <<
+        ba2dd5e6 /home/ubuntu/FinalYards_website/.svelte-kit tmpfs user,noauto,rw,nosuid,nodev,noexec,relatime,size=5120k,uid=1000,gid=1000,inode64 0 0
+      <<
+
+      sudo nano /etc/fstab
+
+      Try it:
+
+      sudo systemctl daemon-reload
+      mount .svelte-kit
+
+      mount | grep \$(pwd)/
+
+      You should see the 'tmpfs' entry.
+
+    2.3 To re-mount after VM restarts
+
+      - either manually do the 'mount .svelte-kit', each time, or add it to your '~/.bashrc' (with full path, of course):
+
+      <<
+        mount $HOME/{your-folder}/.svelte-kit
+      <<
+
+    That's it!
+
+    3. Testing
+
+      3.-
+
+      Clear the contents of persistent mounts:
+
+      rm -rf node_modules/* node_modules/.bin
+
+      3.0 Restart your VM, to also test the persistence of the mounts.
+
+      3.1 You should see two entries with:
+
+      # within your 'npm' project folder
+      $ mount | grep $(pwd)/
+      /dev/sda1 on /home/ubuntu/FinalYards_website/node_modules type ext4 (rw,nosuid,nodev,noexec,relatime,discard,errors=remount-ro,commit=30)
+      ba2dd5e6 on /home/ubuntu/FinalYards_website/.svelte-kit type tmpfs (rw,nosuid,nodev,noexec,relatime,size=5120k,uid=1000,gid=1000,inode64,user=ubuntu)
+
+      3.2 'npm install' and 'npm dev' should have native speed
+
+      rm -rf node_modules/*
+      rm -rf .svelte-kit/*
+
+      time npm install
+      [...]
+
+
 EOF
